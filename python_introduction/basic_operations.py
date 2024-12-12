6f6c9637c06c922008af78f57df58a1a23f70d43
Alazar Zemene <alazarzem2022@gmail.com>
2024-12-12 22:57:48 +0300

    modify

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10;
+number2=5;
+result= number1+number2;
+difference= number1-number2;
+product= number1*number2;
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(result));
+print("Difference of " + str(number1) + " and " + str(number2) + " is " + str(difference));
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(product));
